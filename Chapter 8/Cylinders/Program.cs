c1397de9dff2d184350689bd6133a359d6ddf2b3
masterjed7262@gmail.com <masterjed7262@gmail.com>
2020-05-08 22:22:43 +0800

    added ch8

---
+++ Chapter 8/Cylinders/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cylinders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var h = Convert.ToInt32(Console.ReadLine());
+            var r = Convert.ToInt32(Console.ReadLine());
+            var pi = 3.1415926;
+
+            var volume = pi * r * r * h;
+            var surfacearea = 2 * pi * r * (r + h);
+
+            Console.WriteLine(volume);
+            Console.WriteLine(surfacearea);
+        }
+    }
+}
