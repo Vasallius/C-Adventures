5eb70392333f0147e2f35de928ab210750e769cf
SubtleCoder7262 <masterjed7262@gmail.com>
2020-05-09 00:09:49 +0800

    added ch 10 files.

---
+++ Chapter 10/EvenAndOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EvenAndOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadKey());
+            
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is an even number");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is an odd number");
+            }
+        }   
+    }
+}
---
+++ Chapter 10/evenodd/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace evenodd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+           
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"{number} is an even number");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is an odd number");
+            }
+        }
+    }
+}
