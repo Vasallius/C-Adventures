6aa7a1b5cb33082459b720fa496641f65b861727
SubtleCoder7262 <masterjed7262@gmail.com>
2020-05-09 12:59:34 +0800

    added file.

---
+++ MonthsoftheYear/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MonthsoftheYear
+{   
+    enum Months { January = 1, February = 2, March = 3, April = 4, May = 5, June = 6, July = 7,
+                    August = 8, September = 9, October = 10, November = 11, December = 12}
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number: ");
+            int userinput = Convert.ToInt32(Console.ReadLine());
+
+            Months today = (Months)userinput;
+
+            Console.WriteLine(today);
+        }
+    }
+}
