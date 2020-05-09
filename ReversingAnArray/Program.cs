58ac24ca799c006db5832ecf87bff19f5b8b2904
SubtleCoder7262 <masterjed7262@gmail.com>
2020-05-09 14:43:54 +0800

    fsdfsdfdsfsddxfg

---
+++ ReversingAnArray/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ReversingAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int [] damn2 = Genar(); //damn exists
+            Reverse(damn2);
+            foreach(int item in damn2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int[] Genar()
+        {
+            int[] damn = new[] { 1, 2, 3, 4, 5, 6 };
+            return damn;
+        }
+
+        static int[] Reverse(int [] damn2)
+        {
+            int firstindex = 0;
+            int secondindex = 5;
+
+            while (firstindex < secondindex)
+            {
+                int temp = damn2[firstindex];
+                damn2[firstindex] = damn2[secondindex];
+                damn2[secondindex] = temp;
+
+                firstindex ++;
+                secondindex--;
+            }
+            
+
+            return damn2;
+            
+        }
+    }
+}
