78d8c77cc82d59249afdc3daadb72db52813c296
SubtleCoder7262 <masterjed7262@gmail.com>
2020-05-09 12:22:00 +0800

    revamped dir

---
+++ CastingandOrderofOperations/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CastingandOrderofOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1.0 + 1 + 1.0f;
+            int x = (int)(7 + 3.0 / 4.0 * 2);
+            Console.WriteLine((1 + 1) / 2 * 3);
+        }
+    }
+}
